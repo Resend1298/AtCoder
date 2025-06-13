cd5fbef657c3f043e5b12c1348cc4eba6d8dcd37
pann <i@pann.me>
2025-06-13 12:20:37 +0900

    ABC 409 B

--- ABC/409/B.py
+++ ABC/409/B.py
@@ -1,1 +1,15 @@
-# TODO: review
+from sortedcontainers import SortedList
+
+
+def main():
+	n = int(input())
+	a = SortedList([int(i) for i in input().split()])
+
+	for i in range(n, -1, -1):
+		if n - a.bisect_left(i) >= i:
+			print(i)
+			exit()
+
+
+if __name__ == "__main__":
+	main()

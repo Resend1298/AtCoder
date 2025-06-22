34cd81523477564affc36b75992c9c100113fd74
pann <i@pann.me>
2025-06-22 18:18:50 +0900

    ABC 411 C

--- ABC/411/C.py
+++ ABC/411/C.py
@@ -1,5 +1,3 @@
-# TODO: review
-
 def main():
 	n, q = [int(i) for i in input().split()]
 	a = [int(i) - 1 for i in input().split()]
@@ -9,21 +7,24 @@ def main():
 
 	for i in a:
 		if not black[i]:
-			if n == 1 or (i != 0 and i != n - 1 and not black[i - 1] and not black[i + 1]) or (
-					i == 0 and not black[i + 1]) or (
-					i == n - 1 and not black[i - 1]):
+			# noinspection DuplicatedCode
+			if ((0 < i < n - 1 and not black[i - 1] and not black[i + 1])
+					or n == 1
+					or (i == 0 and not black[i + 1]) or (i == n - 1 and not black[i - 1])):
 				result += 1
-			elif i != 0 and i != n - 1 and black[i - 1] and black[i + 1]:
+			elif 0 < i < n - 1 and black[i - 1] and black[i + 1]:
 				result -= 1
 			black[i] = True
 		else:
-			if n == 1 or (i != 0 and i != n - 1 and not black[i - 1] and not black[i + 1]) or (
-					i == 0 and not black[i + 1]) or (
-					i == n - 1 and not black[i - 1]):
+			# noinspection DuplicatedCode
+			if ((0 < i < n - 1 and not black[i - 1] and not black[i + 1])
+					or n == 1
+					or (i == 0 and not black[i + 1]) or (i == n - 1 and not black[i - 1])):
 				result -= 1
-			elif i != 0 and i != n - 1 and black[i - 1] and black[i + 1]:
+			elif 0 < i < n - 1 and black[i - 1] and black[i + 1]:
 				result += 1
 			black[i] = False
+
 		print(result)
 
 

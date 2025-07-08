80ebe03a23b1f0467a75da6386b157518d1f630e
pann <i@pann.me>
2025-07-08 22:59:25 +0900

    ABC 413 C

--- ABC/413/C.py
+++ ABC/413/C.py
@@ -1,28 +1,30 @@
-# TODO: review
+from collections import deque
+
 
 def main():
 	q = int(input())
 
-	a = []
-	head = 0
-	tail = 0
+	a = deque()
 
 	for _ in range(q):
 		query = [int(i) for i in input().split()]
 
 		match query[0]:
 			case 1:
-				a.append([tail, tail + query[1] - 1, query[2]])
-				tail += query[1]
+				a.append([query[2], query[1]])
 			case 2:
 				result = 0
-				while query[1] > 0 and query[1] >= a[head][1] - a[head][0] + 1:
-					result += a[head][2] * (a[head][1] - a[head][0] + 1)
-					query[1] -= a[head][1] - a[head][0] + 1
-					head += 1
-				if query[1] > 0:
-					result += a[head][2] * query[1]
-					a[head][0] += query[1]
+
+				while query[1] > 0:
+					if a[0][1] <= query[1]:
+						result += a[0][0] * a[0][1]
+						query[1] -= a[0][1]
+						a.popleft()
+					else:
+						result += a[0][0] * query[1]
+						a[0][1] -= query[1]
+						query[1] = 0
+
 				print(result)
 
 

a39a83e73b30c44d245d9ea22e07b531c2f584fa
pann <i@pann.me>
2025-07-22 17:49:32 +0900

    ABC 415 D

--- ABC/415/D.py
+++ ABC/415/D.py
@@ -1,28 +1,18 @@
-# TODO: review
-
 def main():
 	n, m = [int(i) for i in input().split()]
-	diff = []
-	min_a = float("inf")
-	for i in range(m):
-		ab = [int(i) for i in input().split()]
-		diff.append((ab[0] - ab[1], ab[0]))
-		min_a = min(min_a, ab[0])
+	ab = []
+	for _ in range(m):
+		a, b = [int(i) for i in input().split()]
+		ab.append((a - b, a))
 
-	diff.sort()
-
+	ab.sort()
 	result = 0
 
-	for i, j in diff:
+	for i, j in ab:
 		if n >= j:
 			count = (n - j) // i + 1
+			n -= i * count
 			result += count
-			n -= count * i
-		# while n >= j:
-		# 	n -= i
-		# 	result += 1
-		if n < min_a:
-			break
 
 	print(result)
 

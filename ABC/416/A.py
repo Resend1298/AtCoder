198c4c00898aeccbfad993e871cfec70f01d19fa
pann <i@pann.me>
2025-07-28 13:34:40 +0900

    ABC 416 A

--- ABC/416/A.py
+++ ABC/416/A.py
@@ -1,10 +1,10 @@
-# TODO: review
-
 def main():
 	n, l, r = [int(i) for i in input().split()]
+	l -= 1
+	r -= 1
 	s = input()
 
-	print("Yes" if all(i == 'o' for i in s[l - 1:r]) else "No")
+	print("Yes" if all(s[i] == 'o' for i in range(l, r + 1)) else "No")
 
 
 if __name__ == "__main__":

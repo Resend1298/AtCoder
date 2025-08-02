3b4bbb7a181d9f57398e92a284bf1bc247ca23b5
pann <i@pann.me>
2025-08-03 08:01:59 +0900

    ABC 417 C

--- ABC/417/C.py
+++ ABC/417/C.py
@@ -1,5 +1,3 @@
-# TODO: review
-
 from sortedcontainers import SortedList
 
 
@@ -7,15 +5,13 @@ def main():
 	n = int(input())
 	a = [int(i) for i in input().split()]
 
-	tmp_1, tmp_2 = SortedList(), SortedList()
-	for i in range(n):
-		tmp_1.add(i + 1 - a[i])
-		tmp_2.add(i + 1 + a[i])
-
+	# j-i=Ai+Aj -> i+Ai=j-Aj
+	x = [i + a[i] for i in range(n)]
+	y = SortedList([i - a[i] for i in range(n)])
 	result = 0
-	for i in tmp_1:
-		if i in tmp_2:
-			result += tmp_2.count(i)
+
+	for i in x:
+		result += y.count(i)
 
 	print(result)
 

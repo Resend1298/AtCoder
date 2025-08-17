d27591dc45bce2c0c467302f014942321e090f76
pann <i@pann.me>
2025-08-17 16:07:54 +0900

    ABC 419 D

--- ABC/419/D.py
+++ ABC/419/D.py
@@ -1,27 +1,21 @@
-# TODO: review
-
 def main():
 	n, m = [int(i) for i in input().split()]
 	s = input()
 	t = input()
 
-	reverse = [False] * n
-
+	swap = [False] * n
 	for _ in range(m):
 		l, r = [int(i) - 1 for i in input().split()]
-		reverse[l] = not reverse[l]
+		swap[l] = not swap[l]
 		if r + 1 < n:
-			reverse[r + 1] = not reverse[r + 1]
+			swap[r + 1] = not swap[r + 1]
 
 	result = []
-	current = False
+	current_swap = False
 	for i in range(n):
-		if reverse[i]:
-			current = not current
-		if not current:
-			result.append(s[i])
-		else:
-			result.append(t[i])
+		if swap[i]:
+			current_swap = not current_swap
+		result.append(s[i] if not current_swap else t[i])
 
 	print(''.join(result))
 

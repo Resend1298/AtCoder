34acacd19ede46a53a07c70521b5ec1f3a641185
pann <i@pann.me>
2025-08-29 22:23:19 +0900

    ABC 420 A

--- ABC/420/A.py
+++ ABC/420/A.py
@@ -1,10 +1,11 @@
-# TODO: review
-
 def main():
 	x, y = [int(i) for i in input().split()]
 
 	result = (x + y) % 12
-	print(result if result != 0 else 12)
+	if result == 0:
+		result = 12
+
+	print(result)
 
 
 if __name__ == "__main__":

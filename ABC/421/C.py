7c829b7aa3d58eca47b8743cf796a81f1944885b
pann <i@pann.me>
2025-09-01 16:20:09 +0900

    ABC 421 C

--- ABC/421/C.py
+++ ABC/421/C.py
@@ -1,26 +1,20 @@
-# TODO: review
-
 def main():
 	n = int(input())
 	s = input()
 
-	a_index = []
-	b_index = []
-	for i in range(n * 2):
-		if s[i] == 'A':
-			a_index.append(i)
-		else:
-			b_index.append(i)
+	a_index = [i for i in range(n * 2) if s[i] == 'A']
 
-	result_a = 0
+	# how many operations required to get A_A_A_...
+	result_1 = 0
 	for i in range(n):
-		result_a += abs(a_index[i] - 2 * i)
+		result_1 += abs(a_index[i] - i * 2)
 
-	result_b = 0
+	# how many operations required to get _A_A_A...
+	result_2 = 0
 	for i in range(n):
-		result_b += abs(b_index[i] - 2 * i)
+		result_2 += abs(a_index[i] - (i * 2 + 1))
 
-	print(min(result_a, result_b))
+	print(min(result_1, result_2))
 
 
 if __name__ == "__main__":

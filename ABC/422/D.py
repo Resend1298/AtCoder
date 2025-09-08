c09c93eeda711b0f7e832f0b6ed7b97e584cb59e
pann <i@pann.me>
2025-09-08 15:14:32 +0900

    ABC 422 D

--- ABC/422/D.py
+++ ABC/422/D.py
@@ -1,23 +1,21 @@
-# TODO: review
-
 def main():
 	n, k = [int(i) for i in input().split()]
 
-	match k % (2 ** n):
-		case 0:
-			print(0)
-			print(*[k // (2 ** n)] * (2 ** n))
-		case _:
-			print(1)
+	if k % (2 ** n) == 0:
+		print(0)
+		print(*[k // (2 ** n)] * (2 ** n))
+	else:
+		print(1)
 
-			x = [k // 2, k - k // 2]
-			for _ in range(n - 1):
-				new_x = []
-				for i in x:
-					new_x.append(i // 2)
-					new_x.append(i - i // 2)
-				x = new_x
-			print(*x)
+		x = [k]
+		for _ in range(n):
+			new_x = []
+			for i in x:
+				new_x.append(i // 2)
+				new_x.append(i - i // 2)
+			x = new_x
+
+		print(*x)
 
 
 if __name__ == "__main__":

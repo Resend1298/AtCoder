b07e266a5ef25b359dce40df4e6cb40bb85807c0
pann <i@pann.me>
2025-09-28 15:52:59 +0900

    ABC 425 A

--- ABC/425/A.py
+++ ABC/425/A.py
@@ -1,13 +1,7 @@
-# TODO: review
-
 def main():
 	n = int(input())
 
-	result = 0
-	for i in range(1, n + 1):
-		result += (-1) ** i * i ** 3
-
-	print(result)
+	print(sum((-1) ** i * i ** 3 for i in range(1, n + 1)))
 
 
 if __name__ == "__main__":

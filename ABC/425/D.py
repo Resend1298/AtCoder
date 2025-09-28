26c7668cfadc0c6a83cffc242af087a1e7affeb4
pann <i@pann.me>
2025-09-28 22:56:12 +0900

    ABC 425 D

--- ABC/425/D.py
+++ ABC/425/D.py
@@ -1,44 +1,38 @@
-# TODO: review
-
 def main():
 	h, w = [int(i) for i in input().split()]
-	s = [input() for _ in range(h)]
+	s = [list(input()) for _ in range(h)]
 
-	black_1 = set()
 	black_0 = set()
-	result = 0
+	black_1 = set()
 
 	for i in range(h):
 		for j in range(w):
-			if s[i][j] == '#':
-				result += 1
-				continue
+			if s[i][j] == '.':
+				tmp = 0
+				for x, y in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
+					if 0 <= x < h and 0 <= y < w and s[x][y] == '#':
+						tmp += 1
 
-			tmp = 0
-			for x, y in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
-				if 0 <= x < h and 0 <= y < w and s[x][y] == '#':
-					tmp += 1
-
-			if tmp == 1:
-				black_1.add((i, j))
-			elif tmp == 0:
-				black_0.add((i, j))
+				if tmp == 0:
+					black_0.add((i, j))
+				elif tmp == 1:
+					black_1.add((i, j))
 
 	while black_1:
-		black_1_new = set()
+		new_black_1 = set()
 
 		for i, j in black_1:
-			result += 1
+			s[i][j] = '#'
 			for x, y in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
 				if (x, y) in black_0:
 					black_0.remove((x, y))
-					black_1_new.add((x, y))
-				elif (x, y) in black_1_new:
-					black_1_new.remove((x, y))
+					new_black_1.add((x, y))
+				elif (x, y) in new_black_1:
+					new_black_1.remove((x, y))
 
-		black_1 = black_1_new
+		black_1 = new_black_1
 
-	print(result)
+	print(sum(i.count('#') for i in s))
 
 
 if __name__ == "__main__":

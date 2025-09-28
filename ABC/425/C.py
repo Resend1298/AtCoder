905ada2df386576d97936f34bbe68c7d8403ffd4
pann <i@pann.me>
2025-09-28 20:22:29 +0900

    ABC 425 C

--- ABC/425/C.py
+++ ABC/425/C.py
@@ -1,13 +1,11 @@
-# TODO: review
-
 def main():
 	n, q = [int(i) for i in input().split()]
 	a = [int(i) for i in input().split()]
 
 	shift = 0
-	sum_a = [a[0]]
-	for i in range(1, n):
-		sum_a.append(sum_a[-1] + a[i])
+	sum_a = [0]
+	for i in a:
+		sum_a.append(sum_a[-1] + i)
 
 	for _ in range(q):
 		match [int(i) for i in input().split()]:
@@ -15,16 +13,12 @@ def main():
 				shift += c
 				shift %= n
 			case [2, l, r]:
-				l -= 1
-				r -= 1
-				l += shift
-				r += shift
-				l %= n
-				r %= n
+				l = (l - 1 + shift) % n
+				r = (r - 1 + shift) % n
 				if l <= r:
-					print(sum_a[r] - sum_a[l - 1] if l > 0 else sum_a[r])
+					print(sum_a[r + 1] - sum_a[l])
 				else:
-					print(sum_a[r] + sum_a[-1] - sum_a[l - 1])
+					print(sum_a[-1] - sum_a[l] + sum_a[r + 1])
 
 
 if __name__ == "__main__":

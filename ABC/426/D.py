156923a6bfb127f7f92e14e43364eaabba900b1d
pann <i@pann.me>
2025-10-05 21:10:28 +0900

    ABC 426 D

--- ABC/426/D.py
+++ ABC/426/D.py
@@ -1,5 +1,3 @@
-# TODO: review
-
 def solve():
 	n = int(input())
 	s = [int(i) for i in list(input())]
@@ -8,8 +6,7 @@ def solve():
 		print(0)
 		return
 
-	cost_from_left = []
-	cost_from_right = []
+	cost_from_left = [0]
 	count_0 = 0
 	count_1 = 0
 	for i in range(n):
@@ -18,6 +15,8 @@ def solve():
 			continue
 		count_1 += 1
 		cost_from_left.append(2 * count_0 + count_1)
+
+	cost_from_right = [0]
 	count_0 = 0
 	count_1 = 0
 	for i in range(n - 1, -1, -1):
@@ -26,12 +25,11 @@ def solve():
 			continue
 		count_1 += 1
 		cost_from_right.append(2 * count_0 + count_1)
-	result_0 = min(cost_from_left[-1], cost_from_right[-1])
-	for i in range(len(cost_from_right) - 1):
-		result_0 = min(result_0, cost_from_left[-1 - i - 1] + cost_from_right[i])
 
-	cost_from_left = []
-	cost_from_right = []
+	cost_from_right.reverse()
+	result_0 = min(cost_from_left[i] + cost_from_right[i] for i in range(len(cost_from_left)))
+
+	cost_from_left = [0]
 	count_0 = 0
 	count_1 = 0
 	for i in range(n):
@@ -40,6 +38,8 @@ def solve():
 			continue
 		count_0 += 1
 		cost_from_left.append(2 * count_1 + count_0)
+
+	cost_from_right = [0]
 	count_0 = 0
 	count_1 = 0
 	for i in range(n - 1, -1, -1):
@@ -48,9 +48,9 @@ def solve():
 			continue
 		count_0 += 1
 		cost_from_right.append(2 * count_1 + count_0)
-	result_1 = min(cost_from_left[-1], cost_from_right[-1])
-	for i in range(len(cost_from_right) - 1):
-		result_1 = min(result_1, cost_from_left[-1 - i - 1] + cost_from_right[i])
+
+	cost_from_right.reverse()
+	result_1 = min(cost_from_left[i] + cost_from_right[i] for i in range(len(cost_from_left)))
 
 	print(min(result_0, result_1))
 

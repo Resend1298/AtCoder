e134127d3c1dcc50cb266d7addff278b6a1359ee
pann <i@pann.me>
2025-10-12 15:01:14 +0900

    ABC 427 D

--- ABC/427/D.py
+++ ABC/427/D.py
@@ -1,37 +1,22 @@
-# TODO: review
-
 def solve():
 	n, m, k = [int(i) for i in input().split()]
 	s = input()
-	edges = [[] for _ in range(n)]
-	edges_to = [[] for _ in range(n)]
+	edges_rev = [[] for _ in range(n)]
 	for _ in range(m):
 		u, v = [int(i) - 1 for i in input().split()]
-		edges[u].append(v)
-		edges_to[v].append(u)
+		edges_rev[v].append(u)
 
-	target = set([i for i in range(n) if s[i] == 'A'])
-	all_set = set([i for i in range(n)])
+	b_target = set(i for i in range(n) if s[i] == 'B')
+	all_set = set(range(n))
 
-	for i in range(k, 0, -1):
-		target = all_set - target
-		new_target = set()
-		while target:
-			tmp = target.pop()
-			for j in edges_to[tmp]:
-				new_target.add(j)
-		target = all_set - new_target
-		new_target = set()
-		while target:
-			tmp = target.pop()
-			for j in edges_to[tmp]:
-				new_target.add(j)
-		target = new_target
+	for _ in range(k, 0, -1):
+		b_target = set(j for i in b_target for j in edges_rev[i])
+		a_target = all_set - b_target
+		a_target: set[int] = set(j for i in a_target for j in edges_rev[i])
+		b_target = all_set - a_target
 
-	if 0 in target:
-		print("Alice")
-	else:
-		print("Bob")
+	# noinspection PyUnboundLocalVariable
+	print("Alice" if 0 in a_target else "Bob")
 
 
 def main():

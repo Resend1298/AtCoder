8b0b03dd8a555788507c7dec702ba75c10846da5
pann <i@pann.me>
2025-10-12 14:18:49 +0900

    ABC 427 C

--- ABC/427/C.py
+++ ABC/427/C.py
@@ -1,23 +1,20 @@
-# TODO: review
-
 from itertools import combinations
 
 
 def main():
 	n, m = [int(i) for i in input().split()]
-	edges = []
-	for _ in range(m):
-		u, v = [int(i) - 1 for i in input().split()]
-		edges.append((u, v))
+	edges = [[int(i) - 1 for i in input().split()] for _ in range(m)]
 
 	result = float("inf")
 
-	for i in range(1, n // 2 + 1):
-		for j in combinations(range(n), i):
+	for smaller_set_size in range(1, n // 2 + 1):
+		for smaller_set in combinations(range(n), smaller_set_size):
 			tmp_result = 0
-			for x, y in edges:
-				if (x in j and y in j) or (x not in j and y not in j):
+
+			for u, v in edges:
+				if (u in smaller_set and v in smaller_set) or (u not in smaller_set and v not in smaller_set):
 					tmp_result += 1
+
 			result = min(result, tmp_result)
 
 	print(result)

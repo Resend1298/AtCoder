0c5e687d9f4f10024f5db5797b16beec0d91f266
pann <i@pann.me>
2025-10-26 15:06:11 +0900

    ABC 429 B

--- ABC/429/B.py
+++ ABC/429/B.py
@@ -1,10 +1,9 @@
-# TODO: review
-
 def main():
 	n, m = [int(i) for i in input().split()]
 	a = [int(i) for i in input().split()]
 
 	sum_a = sum(a)
+
 	for i in a:
 		if sum_a - i == m:
 			print("Yes")

f6bef58f924c81cb824e9aa9c8e51cd2aba6427c
pann <i@pann.me>
2025-11-08 16:50:21 +0900

    ABC 430 E

--- ABC/430/E.py
+++ ABC/430/E.py
@@ -1,1 +1,18 @@
-# TODO: unsolved
+# PyPy TLE, CPython AC
+
+def solve():
+	a = input()
+	b = input()
+
+	a *= 2
+	print(a.find(b))
+
+
+def main():
+	t = int(input())
+	for _ in range(t):
+		solve()
+
+
+if __name__ == "__main__":
+	main()

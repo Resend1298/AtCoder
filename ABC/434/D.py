753c2857f91042f88c8b3209abdc8c010fc30f79
pann <i@pann.me>
2025-12-01 16:02:19 +0900

    ABC 434 D

--- ABC/434/D.py
+++ ABC/434/D.py
@@ -1,46 +1,55 @@
-# TODO: TLE
-
-from collections import Counter
-
+# CPython TLE, PyPy AC
 
 def main():
 	n = int(input())
-	imos = [[Counter() for _ in range(2000)] for _ in range(2000)]
-	for i in range(n):
-		u, d, l, r = [int(i) - 1 for i in input().split()]
-		imos[u][l].update({i: 1})
+	udlr = [[int(i) - 1 for i in input().split()] for _ in range(n)]
+
+	imos = [[0] * 2000 for _ in range(2000)]
+	for u, d, l, r in udlr:
+		imos[u][l] += 1
 		if d + 1 < 2000 and r + 1 < 2000:
-			imos[d + 1][r + 1].update({i: 1})
-		if r + 1 < 2000:
-			imos[u][r + 1].update({i: -1})
+			imos[d + 1][r + 1] += 1
 		if d + 1 < 2000:
-			imos[d + 1][l].update({i: -1})
-
+			imos[d + 1][l] -= 1
+		if r + 1 < 2000:
+			imos[u][r + 1] -= 1
+	# noinspection DuplicatedCode
 	for i in range(2000):
 		for j in range(1, 2000):
-			imos[i][j].update(imos[i][j - 1])
-			for key in list(imos[i][j].keys()):
-				if imos[i][j][key] == 0:
-					del imos[i][j][key]
+			imos[i][j] += imos[i][j - 1]
+	for j in range(2000):
+		for i in range(1, 2000):
+			imos[i][j] += imos[i - 1][j]
 
-	for i in range(2000):
-		for j in range(1, 2000):
-			imos[j][i].update(imos[j - 1][i])
-			for key in list(imos[j][i].keys()):
-				if imos[j][i][key] == 0:
-					del imos[j][i][key]
-
-	imos_count = [0] * n
-	empty_count = 0
+	not_covered_by_any = 0
 	for i in range(2000):
 		for j in range(2000):
-			if len(imos[i][j]) == 1:
-				imos_count[list(imos[i][j].keys())[0]] += 1
-			elif len(imos[i][j]) == 0:
-				empty_count += 1
+			if imos[i][j] == 0:
+				not_covered_by_any += 1
 
-	for i in imos_count:
-		print(empty_count + i)
+	prefix_sum = [[0] * 2000 for _ in range(2000)]
+	for i in range(2000):
+		for j in range(2000):
+			if imos[i][j] == 1:
+				prefix_sum[i][j] = 1
+	# noinspection DuplicatedCode
+	for i in range(2000):
+		for j in range(1, 2000):
+			prefix_sum[i][j] += prefix_sum[i][j - 1]
+	for j in range(2000):
+		for i in range(1, 2000):
+			prefix_sum[i][j] += prefix_sum[i - 1][j]
+
+	for u, d, l, r in udlr:
+		result = not_covered_by_any
+		result += prefix_sum[d][r]
+		if l - 1 >= 0:
+			result -= prefix_sum[d][l - 1]
+		if u - 1 >= 0:
+			result -= prefix_sum[u - 1][r]
+		if u - 1 >= 0 and l - 1 >= 0:
+			result += prefix_sum[u - 1][l - 1]
+		print(result)
 
 
 if __name__ == "__main__":

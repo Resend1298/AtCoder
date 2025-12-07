ae2d369c7f15827139cb710c061d7764e7e6caa3
pann <i@pann.me>
2025-12-07 13:31:13 +0900

    ABC 435 C

--- ABC/435/C.py
+++ ABC/435/C.py
@@ -1,10 +1,9 @@
-# TODO: review
-
 def main():
 	n = int(input())
 	a = [int(i) for i in input().split()]
 
 	current_range = 1
+
 	for i in range(1, n + 1):
 		if i > current_range:
 			print(i - 1)

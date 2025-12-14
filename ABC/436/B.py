dd6ca9c78090356598f43be01f1c3c63d2c7cf66
pann <i@pann.me>
2025-12-14 23:57:28 +0900

    ABC 436 B

--- ABC/436/B.py
+++ ABC/436/B.py
@@ -1,25 +1,21 @@
-# TODO: review
-
 def main():
 	n = int(input())
 
-	result = [[None] * n for _ in range(n)]
+	result: list[list[int | None]] = [[None] * n for _ in range(n)]
 
-	result[0][(n - 1) // 2] = 1
 	r = 0
 	c = (n - 1) // 2
 	k = 1
+	result[r][c] = k
+
 	for _ in range(n ** 2 - 1):
 		if result[(r - 1) % n][(c + 1) % n] is None:
 			r = (r - 1) % n
 			c = (c + 1) % n
-			k = k + 1
-			result[r][c] = k
 		else:
 			r = (r + 1) % n
-			c = c
-			k = k + 1
-			result[r][c] = k
+		k += 1
+		result[r][c] = k
 
 	for i in result:
 		print(*i)

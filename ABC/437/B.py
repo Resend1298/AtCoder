276a7e73a14de1f0d21a07079289daf95271133b
pann <i@pann.me>
2025-12-21 15:07:21 +0900

    ABC 437 B

--- ABC/437/B.py
+++ ABC/437/B.py
@@ -1,20 +1,9 @@
-# TODO: review
-
 def main():
 	h, w, n = [int(i) for i in input().split()]
-	a = [[int(i) for i in input().split()] for _ in range(h)]
-	b = set(int(input()) for _ in range(n))
+	a = [{int(i) for i in input().split()} for _ in range(h)]
+	b = {int(input()) for _ in range(n)}
 
-	result = 0
-
-	for i in a:
-		tmp = 0
-		for j in i:
-			if j in b:
-				tmp += 1
-		result = max(result, tmp)
-
-	print(result)
+	print(max(len(i & b) for i in a))
 
 
 if __name__ == "__main__":

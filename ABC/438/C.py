55e4acaa0264ebcaa3e8001c6bf19b29df90c3e9
pann <i@pann.me>
2025-12-28 14:07:04 +0900

    ABC 438 C

--- ABC/438/C.py
+++ ABC/438/C.py
@@ -1,1 +1,18 @@
-# TODO: unsolved
+def main():
+	_ = int(input())
+	a = [int(i) for i in input().split()]
+
+	stack = []
+	for i in a:
+		stack.append(i)
+		while len(stack) >= 4 and stack[-1] == stack[-2] == stack[-3] == stack[-4]:
+			stack.pop()
+			stack.pop()
+			stack.pop()
+			stack.pop()
+
+	print(len(stack))
+
+
+if __name__ == "__main__":
+	main()

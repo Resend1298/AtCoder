3b09d87f7ae864b59a9feaded802fcd4be38752b
pann <i@pann.me>
2026-01-18 13:54:46 +0900

    ABC 441 D

--- ABC/441/D.py
+++ ABC/441/D.py
@@ -1,38 +1,31 @@
-# TODO: review
-
 from collections import deque
 
 
 def main():
 	n, m, l, s, t = [int(i) for i in input().split()]
+	# noinspection DuplicatedCode
 	edges = [[] for _ in range(n)]
 	for _ in range(m):
 		u, v, c = [int(i) for i in input().split()]
 		edges[u - 1].append((v - 1, c))
 
 	q = deque()
-	visited = [[set() for _ in range(l + 1)] for _ in range(n)]
-
 	q.append((0, 0, 0))
-	visited[0][0].add(0)
-
 	result = set()
 
 	while q:
-		current, count, cost = q.popleft()
+		current, edge_count, cost = q.popleft()
 
-		if count == l and s <= cost <= t:
+		if edge_count == l and s <= cost <= t:
 			result.add(current + 1)
 			continue
+		if edge_count >= l or cost >= t:
+			continue
 
-		if count < l and cost < t:
-			for i, j in edges[current]:
-				if cost + j not in visited[i][count + 1]:
-					q.append((i, count + 1, cost + j))
-					visited[i][count + 1].add(cost + j)
+		for i, j in edges[current]:
+			q.append((i, edge_count + 1, cost + j))
 
-	result = sorted(result)
-	print(*result)
+	print(*sorted(result))
 
 
 if __name__ == "__main__":

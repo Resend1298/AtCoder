32e2ffa819a0411243f7dabdbc81f4c55442f1c2
pann <i@pann.me>
2026-02-08 18:33:29 +0900

    ABC 444 D

--- ABC/444/D.py
+++ ABC/444/D.py
@@ -1,31 +1,23 @@
-# TODO: review
-
 def main():
-	n = int(input())
+	_ = int(input())
 	a = [int(i) for i in input().split()]
 
-	a_max = max(a)
-	imos = [0] * a_max
+	max_a = max(a)
+	imos = [0] * max_a
 	for i in a:
 		imos[i - 1] += 1
-	imos.reverse()
+	for i in range(max_a - 2, -1, -1):
+		imos[i] += imos[i + 1]
 
-	imos_sum = [imos[0]]
-	for i in range(1, a_max):
-		imos_sum.append(imos_sum[-1] + imos[i])
-
-	current = 0
 	result = []
-	for i in range(a_max - 1, -1, -1):
-		current += imos_sum[i]
-		if current <= 9:
-			result.append(str(current))
-			current = 0
-		else:
-			result.append(str(current % 10))
-			current = current // 10
+	current = 0
+	for i in imos:
+		current += i
+		result.append(str(current % 10))
+		current //= 10
 	if current > 0:
 		result.append(str(current))
+
 	result.reverse()
 	print(''.join(result))
 

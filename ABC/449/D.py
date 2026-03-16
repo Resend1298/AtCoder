47a9b1a37df85299cbfa09d1292a489dbb166d4c
pann <i@pann.me>
2026-03-16 17:28:13 +0900

    ABC 449 D

--- ABC/449/D.py
+++ ABC/449/D.py
@@ -1,41 +1,23 @@
-# TODO: review
-
 def main():
 	l, r, d, u = [int(i) for i in input().split()]
 
 	result = 0
-
 	for y in range(d, u + 1):
+		y = abs(y)
+
 		if y % 2 == 0:
-			if l <= -abs(y) <= abs(y) <= r:
-				result += abs(y) * 2 + 1
-			elif l <= abs(y) <= r:
-				result += abs(y) - l + 1
-			elif l <= -abs(y) <= r:
-				result += r - (-abs(y)) + 1
-			elif -abs(y) <= l <= r <= abs(y):
-				result += r - l + 1
+			result += min(y, r) - max(-y, l) + 1 if min(y, r) >= max(-y, l) else 0
 
 		if y % 2 == 1:
-			y = abs(y) - 1
-		else:
-			y = abs(y)
-		if r >= y + 2:
-			if l <= y + 2:
-				result += (r - (y + 2)) // 2 + 1
-			else:
-				if l % 2 == 0:
-					result += (r - l) // 2 + 1
-				else:
-					result += (r - (l + 1)) // 2 + 1
-		if l <= -y - 2:
-			if r >= -y - 2:
-				result += (-y - 2 - l) // 2 + 1
-			else:
-				if r % 2 == 0:
-					result += (r - l) // 2 + 1
-				else:
-					result += (r - 1 - l) // 2 + 1
+			y -= 1
+		left = max(y + 2, l)
+		right = r
+		if left <= right:
+			result += (right - left + 1) // 2 if not (left % 2 == 0 and right % 2 == 0) else (right - left + 1) // 2 + 1
+		left = l
+		right = min(-y - 2, r)
+		if left <= right:
+			result += (right - left + 1) // 2 if not (left % 2 == 0 and right % 2 == 0) else (right - left + 1) // 2 + 1
 
 	print(result)
 

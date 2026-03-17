5be2f8a2b71845528b939c7eff9a26c139343e8b
pann <i@pann.me>
2026-03-17 22:45:23 +0900

    Refactor stress tester to use tqdm for progress indication

--- Stress Tester/main.py
+++ Stress Tester/main.py
@@ -1,18 +1,21 @@
 SOLVE_FILE = "../ABC/123/A.py"
 
-import subprocess
-import sys
+from itertools import count
+from subprocess import run
+from sys import executable
+
+from tqdm import tqdm
 
 
 def main():
-	while True:
-		with open("test_data.in", "w") as f:
-			subprocess.run([sys.executable, "gen.py"], stdout=f)
+	for _ in tqdm(count()):
+		with open("test_data.in", 'w') as f:
+			run([executable, "gen.py"], stdout=f)
 
-		with open("test_data.in") as f_in, open("test_data_solve.out", "w") as f_out:
-			subprocess.run([sys.executable, SOLVE_FILE], stdin=f_in, stdout=f_out)
-		with open("test_data.in") as f_in, open("test_data_bf.out", "w") as f_out:
-			subprocess.run([sys.executable, "bf.py"], stdin=f_in, stdout=f_out)
+		with open("test_data.in") as f_in, open("test_data_solve.out", 'w') as f_out:
+			run([executable, SOLVE_FILE], stdin=f_in, stdout=f_out)
+		with open("test_data.in") as f_in, open("test_data_bf.out", 'w') as f_out:
+			run([executable, "bf.py"], stdin=f_in, stdout=f_out)
 
 		with open("test_data_solve.out") as f1, open("test_data_bf.out") as f2:
 			assert f1.read() == f2.read()

0d2dd27f3c5a1f58d0fdcd11b1f543f98382a199
pann <i@pann.me>
2026-04-06 11:04:48 +0900

    ABC 452 A

--- ABC/452/A.py
+++ ABC/452/A.py
@@ -1,13 +1,11 @@
-# TODO: review
-
 def main():
 	m, d = [int(i) for i in input().split()]
+	festivals = {(1, 7), (3, 3), (5, 5), (7, 7), (9, 9)}
 
-	match m, d:
-		case (1, 7) | (3, 3) | (5, 5) | (7, 7) | (9, 9):
-			print("Yes")
-		case _:
-			print("No")
+	if (m, d) in festivals:
+		print("Yes")
+	else:
+		print("No")
 
 
 if __name__ == "__main__":

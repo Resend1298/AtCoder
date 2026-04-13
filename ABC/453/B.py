379aee86ca5466076c3b999120256018b3c9aaad
pann <i@pann.me>
2026-04-13 11:07:31 +0900

    ABC 453 B

--- ABC/453/B.py
+++ ABC/453/B.py
@@ -1,13 +1,11 @@
-# TODO: review
-
 def main():
 	t, x = [int(i) for i in input().split()]
 	a = [int(i) for i in input().split()]
 
-	result = [(0, a[0])]
-	for i in range(1, t + 1):
-		if abs(a[i] - result[-1][1]) >= x:
-			result.append((i, a[i]))
+	result = []
+	for i, j in enumerate(a):
+		if i == 0 or abs(j - result[-1][1]) >= x:
+			result.append((i, j))
 
 	for i, j in result:
 		print(i, j)

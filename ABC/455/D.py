ba371f70b2d56d9bb46f0a24a810035d7548e829
pann <i@pann.me>
2026-04-26 18:00:32 +0900

    ABC 455 D

--- ABC/455/D.py
+++ ABC/455/D.py
@@ -1,26 +1,26 @@
-# TODO: review
-
 def main():
 	n, q = [int(i) for i in input().split()]
 
-	father = [i for i in range(n)]
-	top = set(i for i in range(n))
+	father: list[int | None] = [None] * n
+	top = set(range(n))
 
 	for _ in range(q):
 		c, p = [int(i) - 1 for i in input().split()]
 
-		if father[c] != c:
+		if father[c] is not None:
+			# noinspection PyTypeChecker
 			top.add(father[c])
-		father[c] = p
 		top.remove(p)
+		father[c] = p
 
 	result = [0] * n
 	for i in top:
-		result_tmp = 1
-		while father[i] != i:
-			i = father[i]
-			result_tmp += 1
-		result[i] = result_tmp
+		current = 1
+		while father[i] is not None:
+			current += 1
+			# noinspection PyTypeChecker
+			i: int = father[i]
+		result[i] = current
 
 	print(*result)
 

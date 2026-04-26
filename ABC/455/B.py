7bce56a0136e7fc6d5784aa312d9a66f4e8d13c5
pann <i@pann.me>
2026-04-26 14:39:41 +0900

    ABC 455 B

--- ABC/455/B.py
+++ ABC/455/B.py
@@ -1,24 +1,14 @@
-# TODO: review
-
 def main():
 	h, w = [int(i) for i in input().split()]
 	s = [input() for _ in range(h)]
 
 	result = 0
-
 	for h1 in range(h):
 		for h2 in range(h1, h):
 			for w1 in range(w):
 				for w2 in range(w1, w):
-					satisfied = True
-					for i in range(h1, h2 + 1):
-						for j in range(w1, w2 + 1):
-							if s[i][j] != s[h1 + h2 - i][w1 + w2 - j]:
-								satisfied = False
-								break
-						if not satisfied:
-							break
-					if satisfied:
+					if all(s[i][j] == s[h1 + h2 - i][w1 + w2 - j] for i in range(h1, h2 + 1) for j in
+					       range(w1, w2 + 1)):
 						result += 1
 
 	print(result)

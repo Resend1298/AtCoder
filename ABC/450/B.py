6aee5e2deab77861da23f6d5dd5a34caeb9c9f42
pann <i@pann.me>
2026-05-01 13:23:47 +0900

    ABC 450 B

--- ABC/450/B.py
+++ ABC/450/B.py
@@ -1,13 +1,11 @@
-# TODO: review
-
 def main():
 	n = int(input())
 	cost = [[int(i) for i in input().split()] for _ in range(n - 1)]
 
 	for a in range(n - 2):
-		for c in range(a + 2, n):
-			for b in range(a + 1, c):
-				if cost[a][b - a - 1] + cost[b][c - b - 1] < cost[a][c - a - 1]:
+		for b in range(a + 1, n - 1):
+			for c in range(b + 1, n):
+				if cost[a][c - a - 1] > cost[a][b - a - 1] + cost[b][c - b - 1]:
 					print("Yes")
 					exit()
 

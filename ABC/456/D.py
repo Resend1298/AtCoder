ae6e3195280a3aa76d56286a8dda910f9ffbc173
pann <i@pann.me>
2026-05-05 19:09:34 +0900

    ABC 456 D

--- ABC/456/D.py
+++ ABC/456/D.py
@@ -1,39 +1,12 @@
-# TODO: review
-
 def main():
 	s = input()
 
-	rle_s = []
-	pre_char = s[0]
-	pre_count = 1
-	for i in range(1, len(s)):
-		if s[i] == pre_char:
-			pre_count += 1
-		else:
-			rle_s.append((pre_char, pre_count))
-			pre_char = s[i]
-			pre_count = 1
-	rle_s.append((pre_char, pre_count))
+	possibilities_end_with = {'a': 0, 'b': 0, 'c': 0}
+	for i in s:
+		possibilities_end_with[i] = sum(possibilities_end_with.values()) + 1
+		possibilities_end_with[i] %= 998244353
 
-	current = {'a': 0, 'b': 0, 'c': 0}
-	for char, count in rle_s:
-		new = {'a': 0, 'b': 0, 'c': 0}
-
-		for k, v in current.items():
-			if k != char:
-				new[char] += v * count
-
-		new[char] += count
-
-		for k, v in current.items():
-			new[k] += v
-
-		for i in new:
-			new[i] = new[i] % 998244353
-
-		current = new
-
-	print(sum(current.values()) % 998244353)
+	print(sum(possibilities_end_with.values()) % 998244353)
 
 
 if __name__ == "__main__":

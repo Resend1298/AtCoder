b908b925c63192138aed169c1825ac58e0f282dd
pann <i@pann.me>
2026-05-10 16:41:54 +0900

    ABC 457 B

--- ABC/457/B.py
+++ ABC/457/B.py
@@ -1,11 +1,9 @@
-# TODO: review
-
 def main():
 	n = int(input())
-	a = [[int(i) for i in input().split()] for _ in range(n)]
+	a = [[int(i) for i in input().split()][1:] for _ in range(n)]
 	x, y = [int(i) - 1 for i in input().split()]
 
-	print(a[x][y + 1])
+	print(a[x][y])
 
 
 if __name__ == "__main__":

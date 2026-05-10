97402db694bce086018abc92585085fdd575c719
pann <i@pann.me>
2026-05-10 23:01:48 +0900

    ARC 219 ended

---
+++ ARC/219/A.py
@@ -0,0 +1,40 @@
+from sortedcontainers import SortedKeyList
+
+
+def main():
+	n, m = [int(i) for i in input().split()]
+	s = [[int(i) for i in list(input())] for _ in range(n)]
+
+	q = SortedKeyList(key=lambda x: len(x[2]))
+	for i in range(m):
+		for j in [0, 1]:
+			tmp_set = set()
+			for k in range(n):
+				if s[k][i] == j:
+					tmp_set.add(k)
+			q.add((i, j, tmp_set))
+
+	used = [False] * m
+	result = [0] * m
+	remaining = {i for i in range(n)}
+	while q:
+		current_index, current_char, current_set = q.pop()
+		if used[current_index]:
+			continue
+		used[current_index] = True
+		result[current_index] = current_char
+		remaining -= current_set
+		new_q = SortedKeyList(key=lambda x: len(x[2]))
+		for i in q:
+			new_q.add((i[0], i[1], i[2] - current_set))
+		q = new_q
+
+	if remaining:
+		print("No")
+	else:
+		print("Yes")
+		print(''.join(str(i) for i in result))
+
+
+if __name__ == "__main__":
+	main()
---
+++ ARC/219/B.py
@@ -0,0 +1,30 @@
+from sys import setrecursionlimit
+
+
+def recursion(current_index, p, n):
+	if current_index == n:
+		return 1
+
+	if p[current_index] != current_index + 1:
+		return 0
+
+	return ((n - current_index - 1) + recursion(current_index + 1, p, n)) % 998244353
+
+
+def solve():
+	n = int(input())
+	p = [int(i) for i in input().split()]
+
+	setrecursionlimit(10 ** 7)
+	result = recursion(0, p, n)
+	print(result % 998244353)
+
+
+def main():
+	t = int(input())
+	for _ in range(t):
+		solve()
+
+
+if __name__ == "__main__":
+	main()

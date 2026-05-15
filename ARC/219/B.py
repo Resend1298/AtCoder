d20a178d210cbe42938f659471ce3835ccf8c4ca
pann <i@pann.me>
2026-05-15 14:15:34 +0900

    ARC 219 B

--- ARC/219/B.py
+++ ARC/219/B.py
@@ -1,16 +1,14 @@
-# TODO: review
-
 from sys import setrecursionlimit
 
 
-def recursion(current_index, p, n):
-	if current_index == n:
-		return 1
-
-	if p[current_index] != current_index + 1:
+def recursion(p, n, index):
+	if p[index] != index + 1:
 		return 0
 
-	return ((n - current_index - 1) + recursion(current_index + 1, p, n)) % 998244353
+	if index != n - 1:
+		return n - index - 1 + recursion(p, n, index + 1)
+	else:
+		return 1
 
 
 def solve():
@@ -18,8 +16,7 @@ def solve():
 	p = [int(i) for i in input().split()]
 
 	setrecursionlimit(10 ** 7)
-	result = recursion(0, p, n)
-	print(result % 998244353)
+	print(recursion(p, n, 0) % 998244353)
 
 
 def main():

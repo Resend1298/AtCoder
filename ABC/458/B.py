377df5a3c2b7aee308b303bf53dd7f3642de583e
pann <i@pann.me>
2026-05-17 14:55:05 +0900

    ABC 458 B

--- ABC/458/B.py
+++ ABC/458/B.py
@@ -1,13 +1,11 @@
-# TODO: review
-
 def main():
 	h, w = [int(i) for i in input().split()]
 
 	result = [[0] * w for _ in range(h)]
 	for i in range(h):
 		for j in range(w):
-			for new_x, new_y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
-				if 0 <= new_x < h and 0 <= new_y < w:
+			for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
+				if 0 <= x < h and 0 <= y < w:
 					result[i][j] += 1
 
 	for i in result:

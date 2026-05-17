8696e1da62fab6f932ffa80202d876681f8d5a2c
pann <i@pann.me>
2026-05-17 15:00:07 +0900

    ABC 458 C

--- ABC/458/C.py
+++ ABC/458/C.py
@@ -1,16 +1,11 @@
-# TODO: review
-
 def main():
 	s = input()
 
-	s_len = len(s)
-	c_index = [i for i in range(s_len) if s[i] == 'C']
+	c_index = [i for i in range(len(s)) if s[i] == 'C']
 	result = 0
 
 	for i in c_index:
-		l = i
-		r = s_len - i - 1
-		result += min(l, r) + 1
+		result += min(i, len(s) - i - 1) + 1
 
 	print(result)
 

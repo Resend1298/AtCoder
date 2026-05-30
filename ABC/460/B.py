864a20db30548ac8fee8d8077f0573f78102bee8
pann <i@pann.me>
2026-05-30 22:40:34 +0900

    ABC 460 ended

---
+++ ABC/460/B.py
@@ -0,0 +1,23 @@
+def solve():
+	x1, y1, r1, x2, y2, r2 = [int(i) for i in input().split()]
+
+	if r1 > r2:
+		x1, x2 = x2, x1
+		y1, y2 = y2, y1
+		r1, r2 = r2, r1
+
+	if (x2 - x1) ** 2 + (y2 - y1) ** 2 == (r2 - r1) ** 2 or (x2 - x1) ** 2 + (y2 - y1) ** 2 == (r1 + r2) ** 2 or (
+			r2 - r1) ** 2 < (x2 - x1) ** 2 + (y2 - y1) ** 2 < (r1 + r2) ** 2:
+		print("Yes")
+	else:
+		print("No")
+
+
+def main():
+	t = int(input())
+	for _ in range(t):
+		solve()
+
+
+if __name__ == "__main__":
+	main()
---
+++ ABC/460/C.py
@@ -0,0 +1,23 @@
+from sortedcontainers import SortedList
+
+
+def main():
+	n, m = [int(i) for i in input().split()]
+	a = [int(i) for i in input().split()]
+	b = [int(i) for i in input().split()]
+
+	a.sort()
+	b = SortedList(b)
+	result = 0
+
+	for i in a:
+		index = b.bisect_right(2 * i) - 1
+		if index != -1:
+			result += 1
+			b.pop(index)
+
+	print(result)
+
+
+if __name__ == "__main__":
+	main()

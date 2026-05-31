7d710ddc56ec6e20827f4d2b1e87488487735070
pann <i@pann.me>
2026-05-31 12:50:28 +0900

    ABC 460 B

--- ABC/460/B.py
+++ ABC/460/B.py
@@ -1,15 +1,7 @@
-# TODO: review
-
 def solve():
 	x1, y1, r1, x2, y2, r2 = [int(i) for i in input().split()]
 
-	if r1 > r2:
-		x1, x2 = x2, x1
-		y1, y2 = y2, y1
-		r1, r2 = r2, r1
-
-	if (x2 - x1) ** 2 + (y2 - y1) ** 2 == (r2 - r1) ** 2 or (x2 - x1) ** 2 + (y2 - y1) ** 2 == (r1 + r2) ** 2 or (
-			r2 - r1) ** 2 < (x2 - x1) ** 2 + (y2 - y1) ** 2 < (r1 + r2) ** 2:
+	if abs(r1 - r2) ** 2 <= (x2 - x1) ** 2 + (y2 - y1) ** 2 <= (r1 + r2) ** 2:
 		print("Yes")
 	else:
 		print("No")

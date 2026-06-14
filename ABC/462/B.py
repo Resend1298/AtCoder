6a939ed0f94e21643cff0db8de742cbe1e7fcc7a
pann <i@pann.me>
2026-06-14 20:04:29 +0900

    ABC 462 B

--- ABC/462/B.py
+++ ABC/462/B.py
@@ -1,16 +1,14 @@
-# TODO: review
-
 def main():
 	n = int(input())
+	ka = [[int(i) - 1 for i in input().split()] for _ in range(n)]
 
 	result = [[] for _ in range(n)]
 	for i in range(n):
-		a = [int(i) - 1 for i in input().split()]
-		for j in a[1:]:
+		for j in ka[i][1:]:
 			result[j].append(i)
 
-	for i in result:
-		print(len(i), *[j + 1 for j in i])
+	for i in range(n):
+		print(len(result[i]), *[j + 1 for j in result[i]])
 
 
 if __name__ == "__main__":

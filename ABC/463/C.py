269bc24f254210bd1886c320c00b363c3801108f
pann <i@pann.me>
2026-06-22 14:54:37 +0900

    ABC 463 C

--- ABC/463/C.py
+++ ABC/463/C.py
@@ -1,27 +1,30 @@
-# TODO: review
-
-from sortedcontainers import SortedKeyList
+from sortedcontainers import SortedDict
 
 
 def main():
 	n = int(input())
 	hl = [[int(i) for i in input().split()] for _ in range(n)]
-	q = int(input())
+	_ = int(input())
 	t = [int(i) for i in input().split()]
 
-	hl_sl = SortedKeyList(hl, key=lambda x: (x[0], x[1]))
-	changed = {}
-	max_h = hl_sl[-1][0]
-	changed[0] = max_h
+	# only the tallest person for a given L is needed
+	lh = {}
 	for h, l in hl:
-		hl_sl.remove([h, l])
-		if hl_sl:
-			changed[l] = hl_sl[-1][0]
-	changed = SortedKeyList(changed.items(), key=lambda x: x[0])
+		lh[l] = max(lh.get(l, float("-inf")), h)
+	hl = [(h, l) for l, h in lh.items()]
+	hl.sort(key=lambda x: x[1])
+
+	tallest_at_time = {}
+	current_tallest = float("-inf")
+	for h, l in hl[::-1]:
+		tallest_at_time[l + 1] = current_tallest
+		current_tallest = max(current_tallest, h)
+	tallest_at_time[0] = current_tallest
+	tallest_at_time = SortedDict(tallest_at_time)
 
 	for i in t:
-		index = changed.bisect_key_right(i) - 1
-		print(changed[index][1])
+		index = tallest_at_time.bisect_right(i + 1) - 1
+		print(tallest_at_time.peekitem(index)[1])
 
 
 if __name__ == "__main__":

54e81ca13626167602aaa61f251dff447907caec
pann <i@pann.me>
2026-06-28 05:28:05 +0900

    ABC 464 C: review AC

--- ABC/464/C.py
+++ ABC/464/C.py
@@ -1,6 +1,4 @@
-# TODO: review
-
-from collections import deque, Counter
+from collections import Counter, deque
 
 
 def main():
@@ -8,32 +6,27 @@ def main():
 	adb = [[int(i) for i in input().split()] for _ in range(n)]
 
 	color_set = set()
-	color_count = Counter()
+	color_counter = Counter()
 
-	preprocessed_adb = []
+	adb_preprocessed = []
 	for a, d, b in adb:
-		if d == 1:
+		if d == 1 or a == b:
 			color_set.add(b)
-			color_count[b] += 1
-		elif a == b:
-			color_set.add(a)
-			color_count[a] += 1
+			color_counter[b] += 1
 		else:
-			preprocessed_adb.append((a, d, b))
 			color_set.add(a)
-			color_count[a] += 1
-
-	preprocessed_adb.sort(key=lambda x: x[1])
-	preprocessed_adb = deque(preprocessed_adb)
+			color_counter[a] += 1
+			adb_preprocessed.append((a, d, b))
+	adb_preprocessed = deque(sorted(adb_preprocessed, key=lambda x: x[1]))
 
 	for i in range(1, m + 1):
-		while preprocessed_adb and preprocessed_adb[0][1] <= i:
-			a, d, b = preprocessed_adb.popleft()
-			color_count[a] -= 1
-			if color_count[a] == 0:
+		while adb_preprocessed and adb_preprocessed[0][1] <= i:
+			a, d, b = adb_preprocessed.popleft()
+			color_counter[a] -= 1
+			if color_counter[a] == 0:
 				color_set.remove(a)
 			color_set.add(b)
-			color_count[b] += 1
+			color_counter[b] += 1
 
 		print(len(color_set))
 

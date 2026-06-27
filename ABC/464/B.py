24cc739410b27160e263c62089e7cc12c0d612f6
pann <i@pann.me>
2026-06-28 05:17:16 +0900

    ABC 464 B: review AC

--- ABC/464/B.py
+++ ABC/464/B.py
@@ -1,17 +1,15 @@
-# TODO: review
-
 def main():
 	h, w = [int(i) for i in input().split()]
 	c = [input() for _ in range(h)]
 
-	black_index = [(i, j) for i in range(h) for j in range(w) if c[i][j] == '#']
-	black_index_up = min(i for i, _ in black_index)
-	black_index_down = max(i for i, _ in black_index)
-	black_index_left = min(j for _, j in black_index)
-	black_index_right = max(j for _, j in black_index)
+	black = [(i, j) for i in range(h) for j in range(w) if c[i][j] == '#']
+	up = min(i for i, _ in black)
+	down = max(i for i, _ in black)
+	left = min(j for _, j in black)
+	right = max(j for _, j in black)
 
-	for i in range(black_index_up, black_index_down + 1):
-		print(c[i][black_index_left:black_index_right + 1])
+	for i in c[up:down + 1]:
+		print(i[left:right + 1])
 
 
 if __name__ == "__main__":

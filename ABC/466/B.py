62547197123c6427a60905fdd3cff08218daf271
pann <i@pann.me>
2026-07-12 14:36:35 +0900

    ABC 466 B: review

--- ABC/466/B.py
+++ ABC/466/B.py
@@ -1,14 +1,12 @@
-# TODO: review
-
 def main():
 	n, m = [int(i) for i in input().split()]
 	cs = [[int(i) for i in input().split()] for _ in range(n)]
 
-	biggest_by_color = [-1] * m
+	result = [-1] * m
 	for c, s in cs:
-		biggest_by_color[c - 1] = max(biggest_by_color[c - 1], s)
+		result[c - 1] = max(result[c - 1], s)
 
-	print(*biggest_by_color)
+	print(*result)
 
 
 if __name__ == "__main__":

e888f53b07f2cfeb769b0a353796c488d7e5ba84
pann <i@pann.me>
2026-07-19 20:38:22 +0900

    ABC 467 C: review

--- ABC/467/C.py
+++ ABC/467/C.py
@@ -1,26 +1,22 @@
-# TODO: review
-
 def main():
 	n, m = [int(i) for i in input().split()]
 	a = [int(i) for i in input().split()]
 	b = [int(i) for i in input().split()]
 
-	dp = [[0] * 2 for _ in range(n)]
-	dp[0][1 - a[0]] = 1
+	# dp[i][0]: the minimum cost to satisfy all((a[j]+a[j+1])%2==b[j]) for j in range(i) if a[i]==0
+	# dp[i][1]: the minimum cost to satisfy all((a[j]+a[j+1])%2==b[j]) for j in range(i) if a[i]==1
+	dp = [[float("inf")] * 2 for _ in range(n)]
+	dp[0][0] = 0 if a[0] == 0 else 1
+	dp[0][1] = 0 if a[0] == 1 else 1
+
 	for i in range(1, n):
-		match b[i - 1], a[i]:
-			case 0, 0:
-				dp[i][0] = dp[i - 1][0]
-				dp[i][1] = 1 + dp[i - 1][1]
-			case 0, 1:
-				dp[i][0] = 1 + dp[i - 1][0]
-				dp[i][1] = dp[i - 1][1]
-			case 1, 0:
-				dp[i][0] = dp[i - 1][1]
-				dp[i][1] = 1 + dp[i - 1][0]
-			case 1, 1:
-				dp[i][0] = 1 + dp[i - 1][1]
-				dp[i][1] = dp[i - 1][0]
+		match b[i - 1]:
+			case 0:
+				dp[i][0] = dp[i - 1][0] if a[i] == 0 else dp[i - 1][0] + 1
+				dp[i][1] = dp[i - 1][1] if a[i] == 1 else dp[i - 1][1] + 1
+			case 1:
+				dp[i][0] = dp[i - 1][1] if a[i] == 0 else dp[i - 1][1] + 1
+				dp[i][1] = dp[i - 1][0] if a[i] == 1 else dp[i - 1][0] + 1
 
 	print(min(dp[-1]))
 

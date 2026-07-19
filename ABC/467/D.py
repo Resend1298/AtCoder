e3bf40baec882812735c259adc6ea47ea698e1d8
pann <i@pann.me>
2026-07-19 22:25:43 +0900

    ABC 467 D: solve

--- ABC/467/D.py
+++ ABC/467/D.py
@@ -1,1 +1,21 @@
-# TODO: solve
+def solve():
+	px, py, qx, qy, rx, ry, sx, sy = [int(i) for i in input().split()]
+
+	if (qx - px) * (sy - ry) == (sx - rx) * (qy - py) and (
+			(qx - px) * (rx ** 2 + ry ** 2 - sx ** 2 - sy ** 2) != (sx - rx) * (px ** 2 + py ** 2 - qx ** 2 - qy ** 2)
+			or
+			(qy - py) * (rx ** 2 + ry ** 2 - sx ** 2 - sy ** 2) != (sy - ry) * (px ** 2 + py ** 2 - qx ** 2 - qy ** 2)
+	):
+		print("No")
+	else:
+		print("Yes")
+
+
+def main():
+	t = int(input())
+	for _ in range(t):
+		solve()
+
+
+if __name__ == "__main__":
+	main()

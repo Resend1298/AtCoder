4b3e18710d82da480a0fe5f29f0e0d1b07840697
pann <i@pann.me>
2026-07-20 14:16:13 +0900

    ABC 450 C: review

--- ABC/450/C.py
+++ ABC/450/C.py
@@ -1,5 +1,3 @@
-# TODO: review
-
 from collections import deque
 
 
@@ -7,35 +5,31 @@ def main():
 	h, w = [int(i) for i in input().split()]
 	s = [input() for _ in range(h)]
 
-	white = []
-	for i in range(h):
-		for j in range(w):
-			if s[i][j] == '.':
-				white.append((i, j))
-
-	visited = [[False] * w for _ in range(h)]
+	white_cells = [(i, j) for i in range(h) for j in range(w) if s[i][j] == '.']
 	result = 0
+	visited = [[False] * w for _ in range(h)]
 
-	for i, j in white:
+	for i, j in white_cells:
 		if visited[i][j]:
 			continue
 
+		q = deque()
+		q.append((i, j))
 		visited[i][j] = True
-		q = deque([(i, j)])
-		count = True
+		surrounded = True
 
 		while q:
 			x, y = q.popleft()
 
 			if x == 0 or x == h - 1 or y == 0 or y == w - 1:
-				count = False
+				surrounded = False
 
 			for new_x, new_y in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]:
 				if 0 <= new_x < h and 0 <= new_y < w and s[new_x][new_y] == '.' and not visited[new_x][new_y]:
-					visited[new_x][new_y] = True
 					q.append((new_x, new_y))
+					visited[new_x][new_y] = True
 
-		if count:
+		if surrounded:
 			result += 1
 
 	print(result)

3060dd01589632c0cd602a23eb9b3f2f39481aba
pann <i@pann.me>
2026-07-26 16:59:43 +0900

    ABC 468 C: review

--- ABC/468/C.py
+++ ABC/468/C.py
@@ -1,27 +1,16 @@
-# TODO: review
-
 from itertools import permutations
 
 
 def main():
 	n = int(input())
-	p = [int(i) for i in input().split()]
-	q = [int(i) for i in input().split()]
+	p = tuple([int(i) for i in input().split()])
+	q = tuple([int(i) for i in input().split()])
 
 	result = 0
 
 	for i in permutations(range(1, n + 1), n):
-		for j in range(n):
-			if p[j] > i[j]:
-				break
-			if p[j] < i[j]:
-				for k in range(n):
-					if i[k] > q[k]:
-						break
-					if i[k] < q[k]:
-						result += 1
-						break
-				break
+		if p < i < q:
+			result += 1
 
 	print(result)
 

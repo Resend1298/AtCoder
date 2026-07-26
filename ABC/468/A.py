7777e03b5f4d9ace8cc211a2888250e31cd4ee08
pann <i@pann.me>
2026-07-26 16:50:38 +0900

    ABC 468 A: review

--- ABC/468/A.py
+++ ABC/468/A.py
@@ -1,12 +1,10 @@
-# TODO: review
-
 def main():
 	n = int(input())
 	a = [int(i) for i in input().split()]
 
 	result = 0
 	for i in range(n - 2):
-		if a[i] < a[i + 1] and a[i + 1] > a[i + 2]:
+		if a[i] < a[i + 1] > a[i + 2]:
 			result += 1
 
 	print(result)

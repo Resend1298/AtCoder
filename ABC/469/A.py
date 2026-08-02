a57b167db852b8ee4c37eb6536d5446c1fa69a0e
pann <i@pann.me>
2026-08-02 18:12:24 +0900

    ABC 469: mark post-contest TODOs

--- ABC/469/A.py
+++ ABC/469/A.py
@@ -1,3 +1,5 @@
+# TODO: review
+
 def main():
 	n, k = [int(i) for i in input().split()]
 
--- ABC/469/D.py
+++ ABC/469/D.py
@@ -1,3 +1,5 @@
+# TODO: review
+
 def main():
 	n, m = [int(i) for i in input().split()]
 	ab = [[int(i) - 1 for i in input().split()] for _ in range(m)]

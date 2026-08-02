99685b6c9a9866d5f563356750fefc85aeabc4c1
pann <i@pann.me>
2026-08-02 18:22:35 +0900

    ABC 469 B: review

--- ABC/469/B.py
+++ ABC/469/B.py
@@ -1,10 +1,9 @@
-# TODO: review
-
 def main():
 	n = int(input())
 	s = input()
 
 	result = 0
+
 	for i in range(n):
 		if s[i] == 'x' and (i == 0 or s[i - 1] == 'x') and (i == n - 1 or s[i + 1] == 'x'):
 			result += 1

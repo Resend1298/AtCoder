94a3f957a28f1b477171f6a71e505c09284e69eb
pann <i@pann.me>
2026-08-05 15:09:59 +0900

    ABC 469 D: review

--- ABC/469/D.py
+++ ABC/469/D.py
@@ -1,57 +1,23 @@
-# TODO: review
-
 def main():
 	n, m = [int(i) for i in input().split()]
 	ab = [[int(i) - 1 for i in input().split()] for _ in range(m)]
 
-	x = ab[0][0]
-	y = ab[0][1]
-	possible_x = set()
-	possible_y = set()
-	x_possible = True
-	y_possible = True
+	# possible pairs must be in the form of (a0, x) or (b0, x)
+	a0, b0 = ab[0]
+	a0_x = set(range(n))
+	a0_x.remove(a0)
+	b0_x = set(range(n))
+	b0_x.remove(b0)
 
 	for a, b in ab[1:]:
-		if x_possible:
-			if x != a and x != b:
-				if not possible_x:
-					possible_x.add(a)
-					possible_x.add(b)
-				else:
-					if a not in possible_x and b not in possible_x:
-						x_possible = False
-					else:
-						possible_x &= {a, b}
+		if a0 not in (a, b):
+			a0_x &= {a, b}
+		if b0 not in (a, b):
+			b0_x &= {a, b}
 
-		if y_possible:
-			if y != a and y != b:
-				if not possible_y:
-					possible_y.add(a)
-					possible_y.add(b)
-				else:
-					if a not in possible_y and b not in possible_y:
-						y_possible = False
-					else:
-						possible_y &= {a, b}
-
-	result = 0
-	if x_possible:
-		if not possible_x:
-			result += n - 1
-		else:
-			result += len(possible_x)
-	if y_possible:
-		if not possible_y:
-			result += n - 1
-		else:
-			result += len(possible_y)
-
-	if x_possible and y_possible:
-		if (not possible_x) or (not possible_y):
-			result -= 1
-		elif y in possible_x and x in possible_y:
-			result -= 1
-
+	result = len(a0_x) + len(b0_x)
+	if b0 in a0_x:
+		result -= 1
 	print(result)
 
 

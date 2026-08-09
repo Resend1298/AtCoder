f373381af7e232aeef7a2dd4526c9ef5eb96e783
pann <i@pann.me>
2026-08-09 17:04:05 +0900

    ABC 470 C: review

--- ABC/470/C.py
+++ ABC/470/C.py
@@ -1,28 +1,24 @@
-# TODO: review
-
 def main():
 	n, q = [int(i) for i in input().split()]
 
-	current = 0
 	a = [0] * n
-	not_zero = set()
+	current = 0
+	positive_index = set()
 
 	for _ in range(q):
 		match [int(i) for i in input().split()]:
 			case 1, x:
 				current ^= a[x - 1]
 				a[x - 1] += 1
+				positive_index.add(x - 1)
 				current ^= a[x - 1]
-				not_zero.add(x - 1)
 			case 2,:
-				changes = set()
-				for i in not_zero:
+				for i in positive_index.copy():
 					current ^= a[i]
 					a[i] -= 1
-					current ^= a[i]
 					if a[i] == 0:
-						changes.add(i)
-				not_zero -= changes
+						positive_index.remove(i)
+					current ^= a[i]
 
 		print(current)
 

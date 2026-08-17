4bfef9b7aa0341ea8a63b9ab459984d787110152
pann <i@pann.me>
2026-08-17 15:16:12 +0900

    ABC 471 A: review

--- ABC/471/A.py
+++ ABC/471/A.py
@@ -1,11 +1,10 @@
-# TODO: review
-
 def main():
 	a, b = [int(i) for i in input().split()]
 
 	if a + b == 9 or a - b == 9 or a * b == 9 or a == b * 9:
 		print("Nine")
 	else:
+		# noinspection SpellCheckingInspection
 		print("Nein")
 
 
